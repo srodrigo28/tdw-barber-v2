0e9a20e1c75ac59f5ef692da5a3bdb74b158c955
SEBASTIAO RODRIGO <rodrigoexer1@gmail.com>
2025-04-04 00:39:22 -0300

    updated-busca-rapida

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,17 +7,18 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { db } from "@/lib/prisma";
 import { AvatarImage } from "@radix-ui/react-avatar";
-import { SearchIcon } from "lucide-react";
+import { EyeIcon, FootprintsIcon, SearchIcon } from "lucide-react";
 import Image from "next/image";
 
 const Home = async () => {
   // chama menu banco de dados
-  const barbershops = await db.barbershop.findMany({});
-  const popularBarbershops = await db.barbershop.findMany({ // condicional de filtro
-    orderBy: { 
-      name: "asc" 
+  // const barbershops = await db.barbershop.findMany({});
+  const popularBarbershops = await db.barbershop.findMany({
+    // condicional de filtro
+    orderBy: {
+      name: "asc",
     },
-  })
+  });
   // console.log({ barbershops });
   return (
     <div className="flex flex-col h-screen">
@@ -31,31 +32,38 @@ const Home = async () => {
         </h2>
         <p className="text-slate-400">Sexta-feira, 04 de abril.</p>
 
-        {/* BUSCA */}
+        {/* BUSCA RÁPIDA */}
         <div className="flex items-center gap-2 mt-6">
           <Input placeholder="Faça sua busca" />
           <Button size="icon">
-            {" "}
             <SearchIcon />{" "}
           </Button>
         </div>
 
-        <div className="flex gap-2 justify-center mt-5">
-          <Button size="icon" className="w-24">
-            {" "}
-            <SearchIcon /> Cabelo{" "}
+        {/* BOTÕES BUSCA RÁPIDA */}
+        <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
+          <Button size="icon" className="w-fit px-3">
+            <Image src="/cabelo.svg" alt="cabelo" width={16} height={16} />{" "}
+            Cabelo
           </Button>
-          <Button size="icon" className="w-24">
-            {" "}
-            <SearchIcon /> Barba{" "}
+          <Button size="icon" className="w-fit px-3">
+            <Image src="/barba.svg" alt="barba" width={16} height={16} /> Barba
           </Button>
-          <Button size="icon" className="w-44">
-            {" "}
-            <SearchIcon /> Acabamento{" "}
+          <Button size="icon" className="w-fit px-3">
+            <Image src="/acabamento.svg" alt="acabamento" width={16} height={16} />
+            Acabamento
+          </Button>
+          <Button size="icon" className="w-fit px-3">
+            <FootprintsIcon size={16} />
+            Pezinho
+          </Button>
+          <Button size="icon" className="w-fit px-3">
+            <EyeIcon size={16} />
+            Sobrancelha
           </Button>
         </div>
 
-        {/* imagem */}
+        {/* imagem BANNER */}
         <div className="relative w-full h-[150px] mt-5 rounded-md">
           <Image
             src="/banner-01.png"
@@ -106,6 +114,16 @@ const Home = async () => {
           ))}
         </div>
       </div>
+      {/** AREA DE FOOTER */}
+      <footer>
+        <Card className="px-5 py-6 pt-8">
+          <CardContent>
+            <p className="text-sm text-gray-500">
+              @ 2025 Copyright TREINA DEV - Barber
+            </p>
+          </CardContent>
+        </Card>
+      </footer>
     </div>
   );
 };
