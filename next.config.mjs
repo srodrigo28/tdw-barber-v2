567bf32f18e1424fa548033c1bf0b6726dd74ebd
SEBASTIAO RODRIGO <rodrigoexer1@gmail.com>
2025-04-05 00:02:14 -0300

    updated-build

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns:[
-            { 
-                hostname: "utfs.io",
-            },
-        ],
-    },
-    eslint: {
-        // Warning: This allows production builds to successfully complete even if
-        // your project has ESLint errors.
-        ignoreDuringBuilds: false,
+  images: {
+    remotePatterns: [
+      {
+        hostname: "utfs.io",
       },
-};
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
